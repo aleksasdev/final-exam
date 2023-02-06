c3bb2e9d2fe00848f34c4453ba0135dff21e40c7
Aleksas <aleksas.ivanauskas23@gmail.com>
2023-02-06 13:35:09 +0200

    added user context

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import { Route, Routes } from "react-router-dom"
 import { Home } from "@/components/home/Home"
 import { Navbar } from "@/components/main/Navbar"
 import { Footer } from "@/components/main/Footer"
+import { UserProvider } from "@/contexts/UserProvider"
 
 function App() {
 
    return (
       <>
+      <UserProvider>
          <Routes>
             <Route element={<Navbar /> }>
                <Route path="/" element={<Home />} />
             </Route>
          </Routes>
          <Footer />
+      </UserProvider>
       </>
    )
 }
