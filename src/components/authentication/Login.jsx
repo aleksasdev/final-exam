09d17ddf0cd42c9efef2ae5a4e5ac55a84ba2d79
Aleksas <aleksas.ivanauskas23@gmail.com>
2023-02-06 15:35:38 +0200

    login is done

--- src/components/authentication/Login.jsx
+++ src/components/authentication/Login.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ValidationForm, ValidInput, HAVE_VALID_EMAIL,
+   MINIMUM_LENGTH_8 } from '@aleksasdev/validation-form'
+import { Fetcher } from '@aleksasdev/json-api'
+import { DATABASE_URL, USERS_ROUTE } from '@/constants/general';
+import { UserContext } from '@/contexts/UserProvider';
+import { useNavigate } from 'react-router';
 
 export const Login = () => {
+
+   const { loginUser } = useContext(UserContext);
+   const navigator = useNavigate();
+
+   const doLogin = async (values) =>{
+      const [email, password] = values;
+
+      const allUsers = await new Fetcher(DATABASE_URL+USERS_ROUTE).get();
+
+      const matchingUser = Array.isArray(allUsers) && allUsers.find(user => user.email === email);
+      if(!matchingUser){
+         window.alert("Wrong email");
+         return;
+      }
+
+      const isPasswordValid = matchingUser.password === password;
+      if(!isPasswordValid){
+         window.alert("Wrong password");
+         return;
+      }
+
+      loginUser(matchingUser);
+      navigator("/");
+   }
+
    return (
-      <section>Login</section>
+      <section>
+         <ValidationForm onCompleted={doLogin} label="Login" >
+            <p>Email</p>
+            <ValidInput name="email" requirements={[HAVE_VALID_EMAIL]} />
+            <p>Password</p>
+            <ValidInput name="password" requirements={[MINIMUM_LENGTH_8]} type="password" />
+         </ValidationForm>
+      </section>
    )
 }
--- src/contexts/UserProvider.jsx
+++ src/contexts/UserProvider.jsx
@@ -1,5 +1,7 @@
+import { Fetcher } from '@aleksasdev/json-api';
 import React, { useState } from 'react'
 import { createContext } from 'react'
+import { DATABASE_URL, USERS_ROUTE } from '@/constants/general';
 
 export const UserContext = createContext();
 
@@ -7,9 +9,20 @@ export const UserProvider = ({ children }) => {
 
    const [user, setUser] = useState(null);
 
+   const loginUser = async (userObject) =>{
+
+      setUser({
+         id: userObject.id,
+         email: userObject.email,
+         username: userObject.username,
+         avatarUrl: userObject.avatarUrl
+      })
+   }
+
    return (
       <UserContext.Provider value={{
-         user, setUser
+         user, setUser, 
+         loginUser
       }}>
          {children}
       </UserContext.Provider>
