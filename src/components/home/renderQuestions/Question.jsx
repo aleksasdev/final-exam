f257f72ce550a4e701c349c490173d1752e54209
Aleksas <aleksas.ivanauskas23@gmail.com>
2023-02-07 14:15:41 +0200

    added manipulation (edit/delete) container

--- src/components/home/renderQuestions/Question.jsx
+++ src/components/home/renderQuestions/Question.jsx
@@ -1,5 +1,6 @@
 import { UserContext } from '@/contexts/UserProvider';
 import React, { useContext, useEffect, useState } from 'react'
+import { Manipulation } from '../manipulation/Manipulation';
 
 export const Question = ({ data }) => {
 
@@ -20,12 +21,15 @@ export const Question = ({ data }) => {
 			<div className="content">
 				<h1 className="title">{title}</h1>
 				<p className="body">{content}</p>
-				<span className="avatar-wrapper">
-					<img src={ownersDetails.avatarUrl} alt="" />
-					<p className='asked-by'>Asked By</p>
-					<p className='owner-username'>{ownersDetails.username}</p>
-				</span>
 			</div>
+
+			<Manipulation ownerId={ownerId} />
+
+			<span className="avatar-wrapper">
+				<img src={ownersDetails.avatarUrl} alt="" />
+				<p className='asked-by'>Asked By</p>
+				<p className='owner-username'>{ownersDetails.username}</p>
+			</span>
 		</div>
 	)
 }
