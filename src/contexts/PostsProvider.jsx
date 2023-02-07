57fd4cdbb3313f0372afcc62e9ac8fae320c164c
Aleksas <aleksas.ivanauskas23@gmail.com>
2023-02-07 10:01:15 +0200

    further worked on posts context

--- src/contexts/PostsProvider.jsx
+++ src/contexts/PostsProvider.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { createContext } from 'react'
+import { Fetcher } from '@aleksasdev/json-api';
+import { DATABASE_URL } from '@/constants/general';
 
 export const PostsContext = createContext();
 
 export const PostsProvider = ({ children }) => {
 
-  
+	const [posts, setPosts] = useState([]);
 
-   return (
-      <PostsContext.Provider value={{
-         
-      }}>
-         {children}
-      </PostsContext.Provider>
-   )
+	const fetchPosts = async () =>{
+		const allPosts = await new Fetcher(DATABASE_URL+POSTS_ROUTE).get();
+		setPosts(allPosts);
+	}
+
+	useEffect(()=>{
+		fetchPosts();
+	}, [])
+
+	return (
+		<PostsContext.Provider value={{
+			posts, setPosts,
+			fetchPosts
+		}}>
+			{children}
+		</PostsContext.Provider>
+	)
 }
