6d3048e957d91db946f3b1ad6b5f1cd16bdc009d
Aleksas <aleksas.ivanauskas23@gmail.com>
2023-02-07 10:01:36 +0200

    improved question header

--- src/components/home/AskQuestion.jsx
+++ src/components/home/AskQuestion.jsx
@@ -1,13 +1,9 @@
 import React, { useContext } from 'react'
-import { UserContext } from '@/contexts/UserProvider';
 
 export const AskQuestion = () => {
 
-  const { user } = useContext(UserContext);
-
    return (
-      <div className='ask-question-button button-style'>
-         {user ? "Ask Question" : "Please login"}
+      <div>
       </div>
    )
 }
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,15 +1,25 @@
 import React, { useContext } from 'react'
-import { AskQuestion } from './AskQuestion'
 import './home.css'
 import { Filter } from './Filter';
+import { UserContext } from '@/contexts/UserProvider';
+import { Link } from 'react-router-dom';
 
 export const Home = () => {
+
+  const { user } = useContext(UserContext);
 
    return (
       <section id="questions">
          <div className="questions-header">
             <h1 className="all-questions">All Questions</h1>
-            <AskQuestion />
+            {user
+            ?
+               <Link className='ask-question-button button-style' to="/new-question">
+                  Ask Question
+               </Link>
+            :
+               <p className='ask-question-button'>Login to ask a question</p>
+            }
             <p>Questions count</p>
             <Filter />
          </div>
