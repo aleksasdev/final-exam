c134cbac7599e0eb4925ec2ee0acfb6895c4705b
Aleksas <aleksas.ivanauskas23@gmail.com>
2023-02-08 12:54:12 +0200

    added a way to fetch owner details in answer component

--- src/components/home/viewQuestion/answers/Answer.jsx
+++ src/components/home/viewQuestion/answers/Answer.jsx
@@ -1,13 +1,28 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { UserContext } from '@/contexts/UserProvider';
 
 export const Answer = ({ data }) => {
 
    const { content } = data;
+   const { getUserById } = useContext(UserContext);
    const [ownerDetails, setOwnerDetails] = useState(null);
+
+   const getOwnerDetails = async () =>{
+      setOwnerDetails( await getUserById(data.ownerId) );
+   }
+
+   useEffect(()=>{
+      getOwnerDetails();
+   }, [])
 
    return (
       <div className='answer'>
-         <p>{content}</p>
+         {ownerDetails &&
+            <>
+            <p>{content}</p>
+            <p>{ownerDetails.id}</p>
+            </>
+         }
       </div>
    )
 }
